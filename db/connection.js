0c7d06fdf57938732df64a0f9a2a54fed7493e86
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-10-15 19:17:56 +0300

    some update

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 const connection = () => {
   mongoose
-    .connect("mongodb+srv://sarahaa:sarahaa@cluster0.c4t5fu8.mongodb.net/")
+    .connect(
+      "mongodb+srv://sarahaaApp:sarahaaApp@cluster0.c4t5fu8.mongodb.net/"
+    )
     .then(() => console.log("DB connected"))
     .catch((err) => {
       console.log(err);
